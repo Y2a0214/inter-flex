9367c57821c74c0cb5a2a5652a8db761569ce008
Y2a0214 <127768459+Y2a0214@users.noreply.github.com>
2023-05-04 10:31:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let text = document.getElementById("txt")
+
+function gettext(){
+    text.innerHTML = "box1"
+}
+
+let text1 = document.getElementById("txt1")
+
+function gettext1(){
+    text1.innerHTML = "box2"
+}
+
+let text2 = document.getElementById("txt2")
+
+function gettext2(){
+    text2.innerHTML = "box3"
+}
